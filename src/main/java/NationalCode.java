98eb23be0544d099b1d8613fed7c057a24289c8c
parham <p.vafaeei@gmail.com>
2023-02-14 10:35:23 +0330

    update

--- src/main/java/NationalCode.java
+++ src/main/java/NationalCode.java
@@ -1,5 +1,17 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class NationalCode {
 
 private String nationalCode ;
 private Boolean isValid;
+private final LocalDateTime createTime = LocalDateTime.now();
 }
