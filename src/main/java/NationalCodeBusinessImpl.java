e58756e5efcd03618718cda7c118d756087c3a5f
parham <p.vafaeei@gmail.com>
2023-02-14 11:04:31 +0330

    create class NationalCodeBusinessImpl class tjo implement NationalCodeBusiness class

---
+++ src/main/java/NationalCodeBusinessImpl.java
@@ -0,0 +1,23 @@
+
+/*
+ HOW TO :
+
+   This implementation of NationalCodeBusiness created just to work and test in java back-end
+  and using console to show results !
+*/
+public class NationalCodeBusinessImpl implements NationalCodeBusiness{
+    @Override
+    public void validateNationalCode(NationalCode nationalCode) {
+
+    }
+
+    @Override
+    public String getStateCode(NationalCode nationalCode) {
+        return null;
+    }
+
+    @Override
+    public String getPersonUniqueCode(NationalCode nationalCode) {
+        return null;
+    }
+}
