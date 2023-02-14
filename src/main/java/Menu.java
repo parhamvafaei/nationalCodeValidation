3719b8f9f8bedefa23f992a1206eb4d98d1c902e
parham <p.vafaeei@gmail.com>
2023-02-14 11:05:53 +0330

    create Menu class

---
+++ src/main/java/Menu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Menu {
+   static Scanner scanner=new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("please enter nationalCode : ");
+        String nationalCode = scanner.next();
+
+    }
+}
